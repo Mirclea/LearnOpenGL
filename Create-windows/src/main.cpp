ceb3a5976e403e6bec6425c7e35a8a85caf75f57
mirclea <1240619097@qq.com>
2022-09-19 11:25:15 +0800

    '2022/9/19'

---
+++ Create-windows/src/main.cpp
@@ -0,0 +1,48 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+int main()
+{
+	glfwInit();
+	// 设置主要版本和次要版本
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
+	// glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  MAC OS 系统要使用改行命令完成兼容操作
+
+	//创建窗口
+	GLFWwindow* window = glfwCreateWindow(800,600,"learnOpenGL",NULL,NULL);
+	if(window == NULL){
+		std::cout<<"open wrong!"<<std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// 初始化GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+    	return -1;
+	}
+	// 设置视口
+	glViewport(0,0,800,600);
+
+	glfwSetWindowSizeCallback(window,framebuffer_size_callback);
+
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	
+	glfwTerminate(); //最后回收资源
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    glViewport(0, 0, width, height);
+}
